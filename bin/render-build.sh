ab38eb7e9fed7bae67ac5d833ca96195ae2cf027
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2025-09-23 23:34:28 +0100

    Fix Render deployment configuration

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
-
+# exit on error
 set -o errexit
 
+# Install dependencies
 bundle install
 
-bin/rails db:migrate
+# Create and migrate database
+bundle exec rails db:create
+bundle exec rails db:migrate
